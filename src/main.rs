ea3d2185e346a199e555ede4001c19e0c4844347
Sax Dhiut <mail@iyxeyl.me>
2023-09-12 18:29:38 +0400

    feat: add i3 event listener that performs splits automatically based on window geometry

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use i3ipc::{event::Event, reply::Node, I3Connection, I3EventListener, MessageError, Subscription};
+
+fn do_a_split(connection: &mut I3Connection, node: &Node) -> Result<(), MessageError> {
+    let (height, width) = (node.rect.2, node.rect.3);
+    let command = if height > width { "splith" } else { "splitv" };
+    connection.run_command(command).map(|_| ())
+}
+
+fn main() {
+    let mut listener = I3EventListener::connect()
+        .expect("Failed to connect to the i3 IPC interface as a listener");
+    let mut connection =
+        I3Connection::connect().expect("Failed to connect to the i3 IPC interface");
+
+    listener
+        .subscribe(&[Subscription::Window])
+        .expect("Failed to subscribe to the i3 IPC interface");
+
+    for event in listener.listen() {
+        match event.unwrap() {
+            Event::WindowEvent(e) => {
+                if let Err(err) = do_a_split(&mut connection, &e.container) {
+                    eprintln!("Error: {}", err)
+                }
+            }
+            _ => unreachable!(),
+        };
+    }
+}
